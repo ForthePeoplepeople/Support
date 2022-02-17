6b052efd6096e90f5080ec4360d50eb527486b61
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 23:19:19 +0530

    update index.js

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ function login(){
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
-
+window.location.href = "index.html";
+    window.location.replace("http://www.w3schools.com");
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
