e4f006a504a0dc999322d2b6bd0a156cb96c3921
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 18:30:34 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 document.getElementById('loginform').addEventListner("submit",(event)=>{
   event.preventDefault()
 })
+firebase.auth.onAuthStateChangeed((user)=>{
+  if(user){
+    location.replace("viewer/home_viewer.html")
+  }
+})
