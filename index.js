2157cc8c40a466296c11d051303951b6d96e85f2
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 22:09:32 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
 
-document.getElementById('loginform').addEventListner("submit",(event)=>{
-  event.preventDefault()
-})
-firebase.auth.onAuthStateChangeed((user)=>{
-  if(user){
-    location.replace("home_viewer.html")
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
   }
-})
+});
+
 function login(){
-  const email = getElementById('email').value
-  const password = getElementById('password').value
-firebase.auth()signInWithEmailAndPassword(email, password)
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
 }
