45743051b78f63cf3ce3ed2209ef43e62494f99b
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 15:35:21 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function login(){
   var user_email = document.getElementById('email').value;
   var user_password = document.getElementById('password').value;
-  window.alert("working")
+  window.alert( user_password+"working"+user_email)
 }
