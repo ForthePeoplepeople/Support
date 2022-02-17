918b6f325ef01cac80de0ef46ef9d9ce95480135
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-18 00:11:14 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 firebase.auth().onAuthStateChanged(function(user) {
 
-  if (user){
-    location.replace("home_viewer.html");
+  if (user) {
+    location.replace("home_viewer.html")
+    // User is signed in.
+    window.location.href="index.html";
+    
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+    window.location.href="index.html";
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
 });
 
 function login(){
