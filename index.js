063a0a717e24de0c8a1de9bce263be9c06965e69
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 23:12:54 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    window.location.href = "home_viewer.html";
+    window.location.href = "index.html";
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
 
