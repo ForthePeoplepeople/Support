de0551e24306b8341f692ff55e65e20711f5918d
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 23:46:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 firebase.auth().onAuthStateChanged(function(user) {
+
   if (user) {
     // User is signed in.
-    window.location.href="index.html";
+      window.location = 'home_viewer.html'; 
     
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
-
-    var user = firebase.auth().currentUser;
-
-    if(user != null){
-    window.location.href="index.html";
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+    
 
     }
 
