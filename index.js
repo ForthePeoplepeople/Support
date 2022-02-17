5337538f918ec59c8508c585fae56da4563e95b9
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-17 15:59:19 +0530

    update index.js

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
+   window.alert(user_email+"successfully login")
     const uid = user.uid;
-    // ...
+    
   } else {
-    // User is signed out
-    // ...
+    window.alert(error)
   }
 });
 
 function login(){
   var user_email = document.getElementById('email').value;
   var user_password = document.getElementById('password').value;
-  window.alert( user_password+"working"+user_email)
+  
 }
