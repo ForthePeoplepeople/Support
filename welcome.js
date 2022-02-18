e2cec2ddfb4472b216018314764d16bfb9cf18d1
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-18 10:20:29 +0530

    update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,5 +1,12 @@
-firebase.auth.onAuthStateChangeed((user)=>{
-  if(!user){
-    location.replace("login.html")
-  }
+firebase.auth().onAuthStateChanged((user)=>{
+    if(!user){
+        location.replace("index.html")
+    }else{
+        document.getElementById("user").innerHTML = "Hello, "+user.email
+    }
 })
+
+
+function logout(){
+    firebase.auth().signOut()
+}
