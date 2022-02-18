08f8947cec33f847396bd2414bd8a26f6e21ce5c
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-18 13:15:59 +0530

    update viewer.js

--- viewer.js
+++ viewer.js
@@ -10,3 +10,13 @@ firebase.auth().onAuthStateChanged((user)=>{
 function logout(){
     firebase.auth().signOut()
 }
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
