7930001a54e2c5cfb37095cb52812c04079f6049
ForthePeoplepeople <58883167+ForthePeoplepeople@users.noreply.github.com>
2022-02-18 10:19:33 +0530

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,12 @@
-
+var firebaseConfig = {
+    apiKey: "AIzaSyCe-_Hv5RcRLfQIn8_pM37I3L_E-46aSDA",
+    authDomain: "service-bb546.firebaseapp.com",
+    projectId: "service-bb546",
+    storageBucket: "service-bb546.appspot.com",
+    messagingSenderId: "823293259191",
+    appId: "1:823293259191:web:219959b27ab7f9b4fc8c32",
+    measurementId: "G-QJRT2ZP70G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
